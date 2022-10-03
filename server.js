b98aee60639c857ed79d020c93d10f57d6e4f58c
Rajshree326 <2020ugcs046@nitjsr.ac.in>
2022-10-03 18:43:28 +0530

    msg

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const path = require('path');
 const app = express();
 const connectDB = require('./server/database/connection');
 
-
 dotenv.config({ path: 'env/config.env' });
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(morgan('tiny'));
 
