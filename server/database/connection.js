1f657859e79e0e5c70952faba176e8535d5b95a6
Rajshree326 <2020ugcs046@nitjsr.ac.in>
2022-10-04 08:49:17 +0530

    deploying

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: 'C:\Users\HP\OneDrive\Desktop\User management\env\config.env' });
 
 const connectDB = async () => {
   try {
